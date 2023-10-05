69728940c59a1bf69b7c476e0c59b5d0a0a3a762
santacoll19 <collazokel19@gmail.com>
2023-10-05 13:40:43 -0400

    testing

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
-    if len(my_list) == 0:
-        return None
-    max = my_list[0]
-    for i in range(len(my_list)):
-        if my_list[i] > max:
-            max = my_list[i]
-    return max
+def multiple_returns(sentence):
+    if sentence == "":
+        return (0, None)
+    else:
+        return (len(sentence), sentence[0])
