974c73b3de6c9f838e99241e3f287cf15423bd2f
santacoll19 <collazokel19@gmail.com>
2023-10-19 13:52:14 -0400

    testing

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Square class inherited from Rectangle class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square class"""
+
+    def __init__(self, size):
+        """Initializes values"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Calculates area"""
+        return (self.__size * self.__size)
+
+    def __str__(self):
+        """Returns string representation"""
+        return ("[Square] {}/{}".format(self.__size, self.__size))
