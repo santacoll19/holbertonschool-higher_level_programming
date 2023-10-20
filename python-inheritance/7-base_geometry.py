2c985cf7fef1d49e603be3f5fa9d7837fecac325
santacoll19 <collazokel19@gmail.com>
2023-10-19 20:48:07 -0400

    update

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,23 +1,33 @@
 #!/usr/bin/python3
-"""
-This module have a class called BaseGeomtry, that
-contains two function (area, integer_validator)
-"""
+"""Write a class BaseGeometry."""
 
 
-class BaseGeometry:
-    """Class description for Base Geometry"""
+class BaseGeometry():
+    """BaseGeometry class
+    """
 
     def area(self):
-        """Public instance method that raise an Exception"""
-        raise Exception('area() is not implemented')
+        """Public instance method
+
+        Raises:
+            Exception with the message area() is not implemented
+        """
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Public instance method that validates value """
+        """Public instance method that that validates value
+
+        Raises:
+            TypeError: if value is not an integer
+            with the message <name> must be an integer
+            ValueError: if value is less or equal to 0
+            with the message <name> must be greater than 0
+        """
         if type(value) is not int:
-            raise TypeError(name + " must be an integer")
+            raise TypeError("{} must be an integer".format(name))
+
         elif value <= 0:
-            raise ValueError(name + " must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
         else:
             self.name = name
             self.value = value
